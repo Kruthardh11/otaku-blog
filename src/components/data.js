bc21de34bb0663ef2baae6eae0fd91d1ce0adc3e
John Doe <tirunaharipandu@gmail.com>
2023-02-01 14:31:04 +0530

    first

---
+++ src/components/data.js
@@ -0,0 +1,18 @@
+const Blogs = [
+    {
+        id:'1',
+        Title:'Fate Review',
+        image:'https://i.pinimg.com/originals/40/e6/6b/40e66b4593190a817eebebcb05626d2e.jpg',
+        Description:'it is what it is',
+        path: '/fate-review'
+    },
+    {
+        id:'2',
+        Title:'my fav',
+        image:"https://img.etimg.com/thumb/msid-95893645,width-1070,height-580,imgsize-40040,overlay-economictimes/photo.jpg",
+        Description:'my fav of all time',
+        path:'/my-fav'
+    }
+]
+
+export default Blogs;
